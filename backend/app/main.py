bf9ebe5c46abbdfebadf074a1bc45b1ec150db15
DADieAnotherDay <eeshan.agrawal.779@gmail.com>
2026-07-05 19:35:28 +0530

    Fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -56,7 +56,7 @@ app = FastAPI(
 # ── Middleware ─────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]  #settings.allowed_origins_list,
+    allow_origins=["*"],  #settings.allowed_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
